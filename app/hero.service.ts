0355892e895d90e3d31ad21b7d2e9ee753dbc52a
Derrick Bentley <bentleyd@mail.gvsu.edu>
2016-06-26 23:36:59 -0400

    routing completed, separate folders for CSS and Templates

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,11 +5,15 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService{
     getHeroes(){
-    return Promise.resolve(HEROES);
+        return Promise.resolve(HEROES);
     }
     
-getHeroesSlowly(){
-    return new Promise<Hero[]>(resolve =>
-        setTimeout(()=>resolve(HEROES), 2000));
+    getHeroesSlowly(){
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000));
+    }
+    
+    getHero(id:number){
+        return this.getHeroes().then(heroes => heroes.filter(hero => hero.id === id)[0]);
     }
 }
