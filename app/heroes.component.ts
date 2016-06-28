e551c1103076ac1fb27042ed3e4989b3b2417192
Derrick Bentley <bentleyd@mail.gvsu.edu>
2016-06-27 22:36:54 -0400

    mock services and crud functonalities

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -4,19 +4,43 @@ import { Router } from '@angular/router-deprecated';
 import {Hero} from './hero';
 import { HeroService } from './hero.service';
 
+import { HeroDetailComponent } from './hero-detail.component';
+
 @Component({
   selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
-    styleUrls: ['app/heroes.component.css']
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
     title = 'Tour of Heroes';
     heroes: Hero[]
     selectedHero: Hero;
+    addingHero = false;
+    error:any;
 constructor(private heroService: HeroService, private router:Router){};
 
     getHeroes() {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    addHero(){
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero:Hero){
+        this.addingHero = false;
+        if(savedHero){
+            this.getHeroes();
+        }
+    }
+
+    delete(hero:Hero, event:any){
+        event.stopPropagation();
+        this.heroService.delete(hero).then(res => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+        })
     }
     
     ngOnInit(){
